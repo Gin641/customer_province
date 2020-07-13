eb1096ca887627aa8ed67dc254c8b57e0e78711b
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-07-13 17:20:23 +0700

    repository

---
+++ src/main/java/com/codegym/cms/model/Province.java
@@ -0,0 +1,45 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "provinces")
+public class Province {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String Name;
+    @OneToMany(targetEntity = Customer.class)
+    private List<Customer> customers;
+    public Province(){
+    }
+
+    public Province(String name) {
+        this.Name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+}
